5d89d585d2b9c1c08caec85eb24b1bc5d0b384fa
apple <apple@appledeimac.local>
2018-02-07 19:25:59 +0800

    change

---
+++ gitDemo/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  gitDemo
+//
+//  Created by apple on 2018/2/7.
+//  Copyright © 2018年 pp. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"AAA");
+    NSLog(@"AAA");
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
